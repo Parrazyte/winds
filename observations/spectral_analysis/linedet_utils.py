hopefully fixed model loading for cont flux computation for autofit line search

observations/spectral_analysis/linedet_utils.py
@@ -62,18 +62,22 @@ def narrow_line_search(data_cont, suffix,line_search_e=[4,10,0.05],line_search_n
     '''
 
     if data_fluxcont is None:
-        model_load(data_cont)
+        data_cont.load()
     else:
-        model_load(data_fluxcont)
+        data_fluxcont.load()
 
     flux_cont = np.zeros(len(line_search_e_space))
+
+    n_models=len(AllModels.sources.keys())
 
     # here to avoid filling the display with information we're already storing
     # with redirect_stdout(open(os.devnull, 'w')):
 
     for ind_e, energy in enumerate(line_search_e_space):
         AllModels.calcFlux(str(energy - line_search_e[2] / 2) + " " + str(energy + line_search_e[2] / 2))
-        flux_cont[ind_e] = AllData(1).flux[0]
+
+        #summing on all the models including the background because here this normalization will be added to the whole continuum (also with the BG models)
+        flux_cont[ind_e] = sum([AllData(1).flux[6*i] for i in range(n_models)])
 
         # this is required because the buffer is different when redirected
         sys.stdout.flush()
7d3d41aff47b65da6ef8084e6528b27f0fc0f987
Maxime Parra <maxime.parra@univ-grenoble-alpes.fr>
2023-07-22 00:51:27 +0200

    still testing new things and filling the table

--- observations/spectral_analysis/Cyg_X-1.py
+++ observations/spectral_analysis/Cyg_X-1.py
@@ -2,10 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import os
+import numpy as np
 from xspec import AllModels,AllData, Model,Fit,Plot,Spectrum,Xset
 import glob
+import time
 
-from xspec_config_multisp import reset,xPlot,Pset,calc_fit
+from xspec_config_multisp import reset,xPlot,Pset,calc_fit, calc_error, addcomp,delcomp,\
+    display_mod_errors
 from astropy.io import fits
 
 import matplotlib as mpl
@@ -19,8 +22,9 @@ reset()
 Pset(xlog=True)
 Plot.background=True
 
+AllModels.mdefine('mbknpo (max(E,B)-B)/abs(E-B+0.0000001)+(1-(max(E,B)-B)/abs(E-B+0.0000001))*(E/B)^I : mul')
 
-def load_first():
+def load_first(integral=True):
     AllData.clear()
     AllModels.clear()
     os.chdir(os.path.join(startdir,'INTEGRAL'))
@@ -67,26 +71,27 @@ def load_first():
 
     AllData('1:1 NuSTAR/14-06/nu80902318004A01_sr.grp '+
             '2:2 NuSTAR/14-06/nu80902318004B01_sr.grp '+
-            '3:3 INTEGRAL/CygX-1_2651_sum_pha.fits')
+            ('3:3 INTEGRAL/CygX-1_2651_sum_pha.fits' if integral else ''))
 
     AllData(1).response='NuSTAR/14-06/nu80902318004A01_sr.rmf'
     AllData(1).response.arf = 'NuSTAR/14-06/nu80902318004A01_sr.arf'
     AllData(1).background= 'NuSTAR/14-06/nu80902318004A01_bk.pha'
     AllData(1).ignore('bad')
-    AllData(1).ignore('**-3. 70.-**')
+    AllData(1).ignore('**-3. 78.-**')
 
     AllData(2).response='NuSTAR/14-06/nu80902318004B01_sr.rmf'
     AllData(2).response.arf = 'NuSTAR/14-06/nu80902318004B01_sr.arf'
     AllData(2).background= 'NuSTAR/14-06/nu80902318004B01_bk.pha'
     AllData(2).ignore('bad')
-    AllData(2).ignore('**-3. 70.-**')
+    AllData(2).ignore('**-3. 78.-**')
 
-    AllData(3).response = 'INTEGRAL/CygX-1_2651_sum_rbn_rmf.fits'
-    AllData(3).ignore('**-30. 500.-**')
-    AllData(3).ignore('bad')
+    if integral:
+        AllData(3).response = 'INTEGRAL/CygX-1_2651_sum_rbn_rmf.fits'
+        AllData(3).ignore('**-30. 500.-**')
+        AllData(3).ignore('bad')
 
 
-def load_second(NICER='obsid'):
+def load_second(NICER='gtis', integral=True):
 
     AllData.clear()
     AllModels.clear()
@@ -136,10 +141,17 @@ def load_second(NICER='obsid'):
 
     elif NICER == 'gtis':
 
-        NICER_gti_grps = glob.glob('NICER/NICERbyGTI/20-06/**.grp')
+        NICER_gti_grps = np.array(glob.glob('NICER/NICERbyGTI/20-06/**.grp'))
+        #sorting to match Ed's order
+        NICER_gti_grps.sort()
+
+        #reconverting in list to concatenate afterwards
+        NICER_gti_grps=NICER_gti_grps.tolist()
+
 
     #and updating the fits files
     for elem_gti in NICER_gti_grps:
+
         # taking off the grouping of the NICER spectrum to load it from out of the folder
         with fits.open(elem_gti, mode='update') as hdul:
             # if we want to remake a direct group load (but annoying to load in different data groups)
@@ -155,7 +167,8 @@ def load_second(NICER='obsid'):
 
     datas+=NICER_gti_grps
 
-    datas+=['INTEGRAL/CygX-1_2653_sum_pha.fits']
+    if integral:
+        datas+=['INTEGRAL/CygX-1_2653_sum_pha.fits']
 
     AllData(' '.join([str(i+1)+':'+str(i+1)+' '+datas[i] for i in range(len(datas))]))
 
@@ -167,9 +180,11 @@ def load_second(NICER='obsid'):
     AllData(2).response.arf = datas[1].split('_')[0]+'_sr.arf'
     AllData(2).background = datas[1].split('_')[0]+'_bk.pha'
 
+    #there seems to be an issue here so we add a 1s waiting time
+    time.sleep(1)
 
-    AllData(1).ignore('**-3. 70.-**')
-    AllData(2).ignore('**-3. 70.-**')
+    AllData(1).ignore('**-3. 78.-**')
+    AllData(2).ignore('**-3. 78.-**')
 
     for i_gti,elem_gti in enumerate(NICER_gti_grps):
 
@@ -180,8 +195,127 @@ def load_second(NICER='obsid'):
         AllData(i_gti+3).ignore('**-0.5 10.-**')
 
     #loading integral response
-    AllData(3+len(NICER_gti_grps)).response = 'INTEGRAL/CygX-1_2651_sum_rbn_rmf.fits'
-    AllData(3+len(NICER_gti_grps)).ignore('**-30. 500.-**')
+    if integral:
+        AllData(3+len(NICER_gti_grps)).response = 'INTEGRAL/CygX-1_2651_sum_rbn_rmf.fits'
+        AllData(3+len(NICER_gti_grps)).ignore('**-30. 500.-**')
+
+def load_nathan_ref_1():
+    load_first(integral=False)
+    Xset.restore('full_reflection__2.xcm')
+def load_nathan_ref_2():
+    load_second(NICER='gtis',integral=False)
+    Xset.restore('full_reflection__3.xcm')
+def load_nathan_emp_1():
+    load_first(integral=False)
+    Xset.restore('empirical__2.xcm')
+
+    #this shouldn't be here so we're updating it
+    AllModels.systematic=0
+    Fit.perform()
+
+def load_nathan_emp_2():
+    load_second(NICER='gtis',integral=False)
+    Xset.restore('empirical__3.xcm')
+
+def nathan_mod_refl():
+
+    AllModels.clear()
+
+    mod=Model('constant*mtable{nuMLIv1.mod}*gabs*edge*TBfeo*simpl(mbknpo(relxillCp + xillverCp) + diskbb) + gaussian + gaussian')
+
+def nathan_mod_emp(integral=True,systematics=False,custom=False):
+
+    mod=Model('constant*TBfeo(diskbb + smedge*nthComp + laor)')
+
+    #freezing the first constant factor
+    AllModels(1)(1).frozen=True
+
+    #and unfreezing the second one
+    AllModels(2)(1).link=''
+    AllModels(2)(1).frozen=False
+
+
+    #consrtaining the nH values (and freezing if not using NICER data (first obs)
+    AllModels(1)(2).values = 0.7
+
+    if AllData.nGroups<=3:
+        AllModels(1)(2).frozen=True
+    else:
+        AllModels(1)(3).frozen = False
+
+    AllModels(1)(4).values=0.5
+
+    #resticting the diskbb Tin range
+    AllModels(1)(6).values=[0.5,0.005,0.3,0.3,1.,1.]
+
+    #fixing/restricting smedge parameters
+    AllModels(1)(8).values=[8,0.01,7.,7.,9.,9.]
+    AllModels(1)(11).values=7
+    AllModels(1)(11).frozen=True
+
+    #linking nthcomp parameters
+    AllModels(1)(14).link='p6'
+    AllModels(1)(15).values=1
+
+    #setting up laor parameters
+    AllModels(1)(18).values=6.67
+    AllModels(1)(18).frozen=True
+    AllModels(1)(19).frozen=False
+    AllModels(1)(22).frozen=False
+
+    last_group = AllData.nGroups
+
+    #common norm factor for NICER groups if they are there
+    if AllData.nGroups>3:
+        for i_grp in range(3,last_group+(0 if integral else 1)):
+
+            if i_grp==3:
+                AllModels(i_grp)(1).link=''
+                AllModels(i_grp)(1).frozen = False
+            else:
+                AllModels(i_grp)(1).link='p'+str(1+2*AllModels(1).nParameters)
+
+        #putting systematics at 0.01 if for the second observation
+        if systematics:
+            AllModels.systematic=0.01
+
+    #unfreezing the constant factor for the last data group (integral) if it's there
+    if integral:
+        AllModels(last_group)(1).link=''
+        AllModels(last_group)(1).frozen=False
+
+    #first fit without FPMA
+    AllData(1).ignore('3.-7.')
+
+    if custom:
+        delcomp('smedge')
+        AllData(3).ignore('**-2.5')
+
+        #freezing the nH values since it's mostly unconstrained with this energy restriction
+        AllModels(1)(2).frozen=True
+        AllModels(1)(3).frozen=True
+
+    Fit.perform()
+
+    #freezing the second constant factor
+    AllModels(2)(1).frozen=True
+
+    #re-noticing low-E FPMA
+    AllData(1).notice('3.-7.')
+
+    #adding the MLI component behind the constant factor
+    addcomp('mtable{nuMLIv1.mod}',position=2)
+
+    #freezing the second MLI to 1
+    AllModels(2)(2).values=1
+    AllModels(2)(2).frozen=True
+
+    if AllData.nGroups>=2:
+        for i_grp in range(2,last_group+1):
+            #same for every other spectrum
+            AllModels(i_grp)(2).values=1
+            AllModels(i_grp)(2).frozen=True
+
 
 def make_std_mod(fit=False,cut_low_Nustar=True):
 
@@ -191,10 +325,10 @@ def make_std_mod(fit=False,cut_low_Nustar=True):
     AllData(1).ignore('bad')
 
     AllData(2).ignore('bad')
-    AllData(2).ignore('**-3. 70.-**')
+    AllData(2).ignore('**-3. 78.-**')
 
     AllData(3).ignore('bad')
-    AllData(3).ignore('**-3. 70.-**')
+    AllData(3).ignore('**-3. 78.-**')
 
     if cut_low_Nustar:
         AllData(2).ignore('**-4.')
@@ -228,7 +362,6 @@ def make_std_mod(fit=False,cut_low_Nustar=True):
     if fit:
         calc_fit()
 
-
 def make_model_high(fit=False):
 
     AllData.notice('all')
@@ -237,10 +370,10 @@ def make_model_high(fit=False):
     AllData(1).ignore('bad')
 
     AllData(2).ignore('bad')
-    AllData(2).ignore('**-3. 70.-**')
+    AllData(2).ignore('**-3. 78.-**')
 
     AllData(3).ignore('bad')
-    AllData(3).ignore('**-3. 70.-**')
+    AllData(3).ignore('**-3. 78.-**')
 
     #if a NICER set is loaded
     if AllData.nGroups>=4:

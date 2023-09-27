eeb336f6434506fde9deb3991065e81dbd049875
Maxime Parra <maxime.parra@univ-grenoble-alpes.fr>
2023-09-27 14:45:40 +0200

    new improved version considering the axe log scale automatically

--- observations/visualisation/lmplot_uncert.py
+++ observations/visualisation/lmplot_uncert.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from numpy import array, nan_to_num, zeros, transpose, isnan
+import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib as mpl
@@ -9,19 +10,44 @@ from scipy.stats import linregress
 from custom_pymccorrelation import perturb_values
 
 
-def lmplot_uncert_a(ax, x, y, dx, dy, percent=90, distrib='gaussian', nsim=255, linecolor='blue', lw=1.3,
+def lmplot_uncert_a(ax, x, y, dx, dy, percent=90, distrib='gaussian', nsim=1000, linecolor='blue', lw=1.3,
                     intercolor=None,
                     shade_regions=False, return_intercept=True):
     # bandcolor variable
     if intercolor == None:
-        bandcolor = 'light' + linecolor
+        bandcolor = 'lightgrey'
     else:
         bandcolor = intercolor
+
+    log_x=ax.get_xscale()=='log'
+    log_y=ax.get_yscale()=='log'
+
     # switching the format to array to compute the perturbations
     x_arr = array(x)
     y_arr = array(y)
     dx_arr = array(nan_to_num(dx)).T * (percent / 90)
     dy_arr = array(nan_to_num(dy)).T * (percent / 90)
+
+    if log_x:
+        x_arr=np.log10(x_arr)
+
+        #computing the +/- dx effect in log space
+        dx_arr=np.array([np.log10(array(x)+array(dx_arr.T[0])),np.log10(array(x)-array(dx_arr.T[1]))])
+
+        #and applying it
+        dx_arr=np.array([dx_arr[0]-x_arr,x_arr-dx_arr[1]]).T
+
+
+    if log_y:
+        y_arr = np.log10(y_arr)
+
+        # computing the +/- dx effect in log space
+        dy_arr = np.array([np.log10(array(y) + array(dy_arr.T[0])), np.log10(array(y) - array(dy_arr.T[1]))])
+
+        #and applying it
+        dy_arr=np.array([dy_arr[0]-y_arr,y_arr-dy_arr[1]]).T
+
+
     # computing perturbations
     x_pert, y_pert = perturb_values(x_arr, y_arr, dx_arr, dy_arr, Nperturb=nsim)[:2]
     x_pert = x_pert.astype(float)
@@ -43,8 +69,14 @@ def lmplot_uncert_a(ax, x, y, dx, dy, percent=90, distrib='gaussian', nsim=255, 
     for elem_children in ax_children_regplot:
         elem_children.remove()
 
+    #we make this one in linear to ensure no issue for the sampling created from regplot
+    fig_new,ax_new=plt.subplots()
+
+    ax_new.set_xlim(np.log10(ax.get_xlim()))
+    ax_new.set_ylim(np.log10(ax.get_ylim()))
+
     # updating the list of children to be preserved
-    ax_children_init = ax.get_children()
+    ax_children_init = ax_new.get_children()
 
     slope_vals = zeros(nsim)
     intercept_vals = zeros(nsim)
@@ -53,6 +85,7 @@ def lmplot_uncert_a(ax, x, y, dx, dy, percent=90, distrib='gaussian', nsim=255, 
     plt.xlim(plt.xlim())
     plt.ylim(plt.ylim())
     bound_inter = array([None] * nsim)
+
     with tqdm(total=nsim) as pbar:
         for i in range(nsim):
 
@@ -66,9 +99,9 @@ def lmplot_uncert_a(ax, x, y, dx, dy, percent=90, distrib='gaussian', nsim=255, 
             # computing a dataframe set from an iteration of perturbed values
             df_pert = pd.DataFrame(data=array([x_pert[i], y_pert[i]]).T, columns=['x_pert', 'y_pert'])
             # computing the regression plot on the current axis
-            sns.regplot(x=x_pert[i], y=y_pert[i], ax=ax, truncate=False, ci=90)
+            sns.regplot(x=x_pert[i], y=y_pert[i], ax=ax_new, truncate=False, ci=90)
             # fetching the newly added elements to the axis list
-            ax_children_regplot = [elem for elem in ax.get_children() if elem not in ax_children_init]
+            ax_children_regplot = [elem for elem in ax_new.get_children() if elem not in ax_children_init]
 
             for elem_children in ax_children_regplot:
                 # removing everything but the line interval
@@ -82,6 +115,7 @@ def lmplot_uncert_a(ax, x, y, dx, dy, percent=90, distrib='gaussian', nsim=255, 
                     else:
                         # storing the points of this interval
                         points_inter = elem_children.get_paths()[0].to_polygons()[0].T
+
                         # computing the sampling fo the polygons (number of abscisses used as boundaries)
                         # note: the start and finish point are doubled, so we need to take them off
                         reg_sampling = int(len(points_inter[0]) / 2 - 1)
@@ -95,8 +129,10 @@ def lmplot_uncert_a(ax, x, y, dx, dy, percent=90, distrib='gaussian', nsim=255, 
                         elem_children.remove()
             pbar.update()
 
+    plt.close(fig_new)
+
     if not shade_regions:
-        # now that we ahve the array, we re-organize it into something regular, then transpose and sort it to get the distribution
+        # now that we have the array, we re-organize it into something regular, then transpose and sort it to get the distribution
         # of each boundary
         bound_inter = array([elem for elem in bound_inter])
         # transposing into #low-high curve / point / iteration
@@ -107,7 +143,15 @@ def lmplot_uncert_a(ax, x, y, dx, dy, percent=90, distrib='gaussian', nsim=255, 
         low_curve = array([bound_inter[0][i][round((1 - percent / 100) * nsim)] for i in range(reg_sampling)])
         high_curve = array([bound_inter[1][i][round((percent / 100) * nsim)] for i in range(reg_sampling)])
         # filling the region
-        ax.fill_between(abs_inter, low_curve, high_curve, color=bandcolor)
+
+        if log_x:
+            abs_inter=10**abs_inter
+
+        if log_y:
+            low_curve=10**low_curve
+            high_curve=10**high_curve
+
+        ax.fill_between(abs_inter, low_curve, high_curve, color=bandcolor,zorder=0)
 
     uncert_arr = array([[None, None, None]] * 2)
 
@@ -131,15 +175,32 @@ def lmplot_uncert_a(ax, x, y, dx, dy, percent=90, distrib='gaussian', nsim=255, 
 
     #plotting the median line with the median value of the intercept and coefficient
 
-    #fetching two points to create the line in between
-    x_line=ax.get_xlim()
+    #fetching a sample of points from the limits of the ax to create the line in between
+    #note
+    if log_x:
+        x_line=np.linspace(np.log10(ax.get_xlim()[0]),np.log10(ax.get_xlim()[1]),500)
+    else:
+        x_line=np.linspace(ax.get_xlim()[0],ax.get_xlim()[1],500)
 
     #locking the ax to avoid resizing when plotting the next line
     ax.set_xlim(ax.get_xlim())
     ax.set_ylim(ax.get_ylim())
 
+    y_line=(x_line-x_intercept)*uncert_arr[0][0]+uncert_arr[1][0]
+
+    #converting to powers if in log space
+    if log_x:
+        x_line_plot=10**x_line
+    else:
+        x_line_plot=x_line
+
+    if log_y:
+        y_line_plot=10**y_line
+    else:
+        y_line_plot=y_line
+
     #plotting the line
-    plt.plot(x_line,(x_line-x_intercept)*uncert_arr[0][0]+uncert_arr[1][0],lw=lw,color=linecolor)
+    plt.plot(x_line_plot,y_line_plot,lw=lw,color=linecolor)
 
     if return_intercept:
         return uncert_arr, x_intercept

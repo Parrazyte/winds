b296cedd1eb9e198578e98daacd179d876e6b090
Maxime Parra <maxime.parra@univ-grenoble-alpes.fr>
2025-03-15 18:55:43 +0900

    more options for xrism_ls

--- observations/spectral_analysis/XRISM_utils.py
+++ observations/spectral_analysis/XRISM_utils.py
@@ -1,7 +1,8 @@
 
-from xspec import Xset, Plot, Fit
+from xspec import Xset, Plot, Fit,AllData, AllModels
 from xspec_config_multisp import freeze, allmodel_data
 from linedet_utils import narrow_line_search,plot_line_search
+import matplotlib.pyplot as plt
 import dill
 import numpy as np
 import glob
@@ -9,10 +10,12 @@ import os
 
 def rebinv_xrism(grp_number=1,sigma=2):
     Plot.setRebin(sigma, 5000, grp_number)
-def xrism_ls(baseload,low_e,high_e,plot_suffix,e_step=5e-3,e_sat_low_indiv=[1.5,1.5],resolve_dg=1,
-             force_ylog_ratio=True,ratio_bounds=[0.05,20],title=True):
+def xrism_ls(baseload,low_e,high_e,plot_suffix="",bound_around=0.1,e_step=5e-3,lw=5e-3,
+             e_sat_low_indiv=[1.5,1.5],resolve_dg=1,rebinv=20,
+             force_ylog_ratio=True,ratio_bounds=[0.5,2],title=True):
 
     '''
+
     Simple wrapper to compute a line search and make an associated plot with XRISM
 
     baseload should be an XCM file with both the file and model
@@ -26,21 +29,35 @@ def xrism_ls(baseload,low_e,high_e,plot_suffix,e_step=5e-3,e_sat_low_indiv=[1.5,
     prev_chatter=Xset.chatter
     Xset.chatter=1
 
+    AllData.clear()
+    AllModels.clear()
+
     Xset.restore(baseload)
-    rebinv_xrism(resolve_dg)
+    rebinv_xrism(1,sigma=rebinv)
+
     freeze()
 
+
+    if bound_around is not None:
+        AllData.ignore('**-'+str(low_e-bound_around)+' '+str(high_e+bound_around)+'-**')
+
+
     mod_ls=allmodel_data()
-    narrow_out_val=narrow_line_search(mod_ls,'mod_ls',e_sat_low_indiv,[low_e,high_e,e_step],
-                                      line_cont_range=[low_e,high_e],scorpeon=False)
+    narrow_out_val=narrow_line_search(mod_ls,'mod_ls',
+                                      e_sat_low_indiv,[low_e,high_e,e_step],
+                                      line_cont_range=[low_e,high_e],lw=lw,scorpeon=False)
 
+    lw_str='_lw_'+str(round(lw*1e3))
 
-    with open(baseload[:baseload.rfind('.')]+'_narrow_out_'+str(low_e)+'_'+str(high_e)+'.dill','wb+') as f:
+    with open(baseload[:baseload.rfind('.')]+'_narrow_out_'+str(low_e)+'_'+str(high_e)+lw_str+'.dill',
+              'wb+') as f:
         dill.dump(narrow_out_val,f)
 
-    plot_line_search(narrow_out_val, './', 'XRISM', suffix='', save=True,
+    plt.ioff()
+    plot_line_search(narrow_out_val, './', 'XRISM', suffix=lw_str, save=True,
                      epoch_observ=[plot_suffix], format='pdf',
                      force_ylog_ratio=force_ylog_ratio,ratio_bounds=ratio_bounds,title=title)
+    plt.ion()
 
     Xset.chatter=prev_chatter
 

a645681d559ed548b1df8c87b4f8272e0ac02d57
parrazyte <maxime.parrastro@gmail.com>
2026-06-02 14:05:35 +0900

    added SQUDE compa plots and Q-shape detectability plots

--- observations/spectral_analysis/lines_simu_plots.py
+++ observations/spectral_analysis/lines_simu_plots.py
@@ -8,7 +8,9 @@ import glob
 import pandas as pd
 from general_tools import ravel_ragged
 from line_simus_tools import loglog_regressor
-
+import matplotlib.ticker as tkr
+from scipy.stats import linregress
+from matplotlib.collections import LineCollection
 import getpass
 username=getpass.getuser()
 
@@ -432,7 +434,7 @@ elif mode=='combi':
                 ('_vert' if vert else '')+'.pdf'))
 
 
-if 0:
+def compa_plots_XMM_A025():
     #handmade version for XMM AO25
 
     mpl.rcParams.update({'font.size': 14})
@@ -499,3 +501,373 @@ if 0:
     plt.legend()
     plt.savefig('fig_linedet_verr.pdf')
     plt.close()
+
+
+def compa_plots_SQUDE_XRB():
+    #handmade version for SQUDE XRB proposal
+
+    mpl.rcParams.update({'font.size': 14})
+
+
+    os.chdir('/media/parrazyte/crucial_SSD/Observ/highres/simu_SQUDE_propal/plot_NH')
+
+    plt.figure(figsize=(4.5,4),layout='constrained')
+
+    ax=plt.gca()
+
+    load_NH_RGS=np.loadtxt('RGS_photo_nh_lim_mod_fit_hard_2e21_50ks_20_iter_line_FeKa26abs_flux_1_100_5_in_2p0_10p0_keV_mod_pion_abs_NS_0.005.txt')
+
+    load_NH_XRISM=np.loadtxt('XRISM_photo_nh_lim_mod_mod_bs_50ks_20_iter_line_FeKa26abs_flux_1_100_5_in_2p0_10p0_keV_mod_pion_abs_NS_0.005.txt')
+
+    load_NH_SQUDE_hot=np.loadtxt('SQUDE_hot_photo_nh_lim_mod_fit_hard_2e22_50ks_41_iter_line_FeKa26abs_flux_0.1_1000_9_in_2p0_10p0_keV_mod_pion_abs_NS_0.005.txt')
+    load_NH_SQUDE_warm=np.loadtxt('SQUDE_warm_photo_nh_lim_mod_fit_hard_2e22_50ks_41_iter_line_FeKa26abs_flux_0.1_1000_9_in_2p0_10p0_keV_mod_pion_abs_NS_0.005.txt')
+
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.xlabel(r'NH threshold for 3$\sigma$ detection (cm$^{-2}$)')
+    plt.ylabel(r'Observed Flux (erg s$^{-1}$ cm$^{-2}$)')
+
+    plt.axvline(1e21, color='grey', lw=0.5)
+    plt.axvline(1e22, color='grey', lw=0.5)
+    plt.axvline(1e23, color='grey', lw=0.5)
+
+
+
+    #for SQUDE warm
+    loglog_regressor(load_NH_SQUDE_warm.T[-1]*1e22, load_NH_SQUDE_warm.T[0],sampl_Y=load_NH_XRISM.T[0],
+                     ax=plt.gca(), color='cyan',
+                     marker='+',ls='--',label_plot='SQUDE\nwarm')
+
+    #for SQUDE hot we do the regression only from the brightest point with NH=10 and upwards
+    loglog_regressor(load_NH_SQUDE_hot.T[-1][5:]*1e22, load_NH_SQUDE_hot.T[0][5:],sampl_Y=load_NH_XRISM.T[0],
+                     ax=plt.gca(), color='orange',
+                     marker='+',ls='--',label_plot='SQUDE\nhot')
+
+    #for XRISM
+    loglog_regressor(load_NH_XRISM.T[-1]*1e22, load_NH_XRISM.T[0], ax=plt.gca(), color='red',
+                     marker='+',ls='--',label_plot='XRISM\nhot')
+
+
+    interp_vals_RGS=loglog_regressor(load_NH_RGS.T[-1][:-1]*1e22, load_NH_RGS.T[0][:-1], ax=plt.gca(),
+                                 color='blue', marker='+',ls='--',label_plot='RGS\nwarm',return_vals=True)
+
+    #final scatter for the RGS systematics dominated zone
+    stat_y=load_NH_RGS.T[0][-2:]
+    stat_x = [interp_vals_RGS[0][-1],interp_vals_RGS[0][-1]]
+
+    ax.scatter(stat_x[-1],stat_y[-1],marker='+', color='blue', s=100)
+    ax.plot(stat_x,stat_y,ls='--', color='blue', lw=1, alpha=1., label='')
+
+    # mpl.rcParams.update({'font.size': 8})
+    # plt.legend()
+    # plt.xlim(1e20,6.8e23)
+    plt.savefig('fig_linedet_NH_SQUDE.pdf')
+    plt.savefig('fig_linedet_NH_SQUDE.png',dpi=300)
+    plt.close()
+
+
+
+    ##VSHIFT ERROR
+    os.chdir('/media/parrazyte/crucial_SSD/Observ/highres/simu_SQUDE_propal/plot_verr')
+
+    mpl.rcParams.update({'font.size': 14})
+    plt.figure(figsize=(4.5,4),layout='constrained')
+
+    ax=plt.gca()
+
+    load_verr_RGS=np.loadtxt('RGS_photo_vshift_err_mod_fit_hard_2e21_50ks_20_iter_line_FeKa26abs_flux_1_100_5_in_2p0_10p0_keV_mod_pion_abs_NS_0.005.txt')
+
+    load_verr_XRISM=np.loadtxt('XRISM_1e23_photo_vshift_err_mod_fit_hard_2e22_50ks_41_iter_line_FeKa26abs_flux_1_100_5_in_2p0_10p0_keV_mod_pion_abs_NS_0.005.txt')
+
+    load_verr_SQUDE_hot=np.loadtxt('SQUDE_hot_photo_vshift_err_mod_fit_hard_2e22_50ks_41_iter_line_FeKa26abs_flux_1_100_5_in_2p0_10p0_keV_mod_pion_abs_NS_0.005.txt')
+    load_verr_SQUDE_warm=np.loadtxt('SQUDE_warm_photo_vshift_err_mod_fit_hard_2e22_50ks_41_iter_line_FeKa26abs_flux_1_100_5_in_2p0_10p0_keV_mod_pion_abs_NS_0.005.txt')
+
+
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.xlabel(r'3$\sigma$ velocity shift error (km s$^{-1}$)')
+    plt.ylabel(r'Observed Flux (erg s$^{-1}$ cm$^{-2}$)')
+
+    #for SQUDE warm
+    loglog_regressor(load_verr_SQUDE_warm.T[-1], load_verr_SQUDE_warm.T[0],
+                     ax=plt.gca(), color='cyan',
+                     marker='+',ls='--',label_plot='SQUDE\nwarm')
+
+    #for SQUDE hot we do the regression only from the brightest point with NH=10 and upwards
+    loglog_regressor(load_verr_SQUDE_hot.T[-1][2:], load_verr_SQUDE_hot.T[0][2:],
+                     ax=plt.gca(), color='orange',
+                     marker='+',ls='--',label_plot='SQUDE\nhot')
+
+    loglog_regressor(load_verr_XRISM.T[-1], load_verr_RGS.T[0], ax=plt.gca(), color='red', marker='+',ls='--',label_plot='XRISM\nhot')
+
+    loglog_regressor(load_verr_RGS.T[-1], load_verr_RGS.T[0], ax=plt.gca(), color='blue', marker='+',ls='--',label_plot='RGS\nwarm')
+    mpl.rcParams.update({'font.size': 9})
+    plt.legend()
+
+    plt.savefig('fig_linedet_verr_SQUDE.pdf')
+    plt.savefig('fig_linedet_verr_SQUDE.png',dpi=300)
+
+    plt.close()
+
+
+
+def colored_line(x, y, c, ax, **lc_kwargs):
+
+    """
+    from https://matplotlib.org/stable/gallery/lines_bars_and_markers/multicolored_line.html
+
+    Plot a line with a color specified along the line by a third value.
+
+    It does this by creating a collection of line segments. Each line segment is
+    made up of two straight lines each connecting the current (x, y) point to the
+    midpoints of the lines connecting the current point with its two neighbors.
+    This creates a smooth line with no gaps between the line segments.
+
+    Parameters
+    ----------
+    x, y : array-like
+        The horizontal and vertical coordinates of the data points.
+    c : array-like
+        The color values, which should be the same size as x and y.
+    ax : Axes
+        Axis object on which to plot the colored line.
+    **lc_kwargs
+        Any additional arguments to pass to matplotlib.collections.LineCollection
+        constructor. This should not include the array keyword argument because
+        that is set to the color argument. If provided, it will be overridden.
+
+    Returns
+    -------
+    matplotlib.collections.LineCollection
+        The generated line collection representing the colored line.
+    """
+    if "array" in lc_kwargs:
+        warnings.warn('The provided "array" keyword argument will be overridden')
+
+    # Default the capstyle to butt so that the line segments smoothly line up
+    default_kwargs = {"capstyle": "butt"}
+    default_kwargs.update(lc_kwargs)
+
+    # Compute the midpoints of the line segments. Include the first and last points
+    # twice so we don't need any special syntax later to handle them.
+    x = np.asarray(x)
+    y = np.asarray(y)
+    x_midpts = np.hstack((x[0], 0.5 * (x[1:] + x[:-1]), x[-1]))
+    y_midpts = np.hstack((y[0], 0.5 * (y[1:] + y[:-1]), y[-1]))
+
+    # Determine the start, middle, and end coordinate pair of each line segment.
+    # Use the reshape to add an extra dimension so each pair of points is in its
+    # own list. Then concatenate them to create:
+    # [
+    #   [(x1_start, y1_start), (x1_mid, y1_mid), (x1_end, y1_end)],
+    #   [(x2_start, y2_start), (x2_mid, y2_mid), (x2_end, y2_end)],
+    #   ...
+    # ]
+    coord_start = np.column_stack((x_midpts[:-1], y_midpts[:-1]))[:, np.newaxis, :]
+    coord_mid = np.column_stack((x, y))[:, np.newaxis, :]
+    coord_end = np.column_stack((x_midpts[1:], y_midpts[1:]))[:, np.newaxis, :]
+    segments = np.concatenate((coord_start, coord_mid, coord_end), axis=1)
+
+    lc = LineCollection(segments, **default_kwargs)
+    lc.set_array(c)  # set the colors of each segment
+
+    return ax.add_collection(lc)
+
+def Q_shape_col(val_csv_warm=None,val_csv_hot=None,
+                factor_soft=2,
+                col_warm='magma_r',
+                col_hot='viridis_r',
+                d_kpc=8,M_CO=8,
+                ledd_lowbranch=0.01,ledd_highbranch=0.1,lw=10,
+                transi_width_margin=0.0065,
+                figsize=(6,4)):
+
+    '''
+    Computes the Q-shape evolution of a given parameter according to a sampling of values for a given flux
+
+    val_csv_warm: csv containing the values of the constraints with warm absorber with the hard state SED,
+    val_csv_hot: csv containing the values of the constraints with a hot absorber with a hard state SED,
+
+    factor_soft: flux multiplier for soft state SEDs. Used to draw the vertical line of the soft state and
+                the horizontal lines linking the two together.
+    col: colormap
+    d_kpc,M_CO: used to normalize the position of the Q-shape
+
+    example:
+    Q_shape_col('photo_nh_lim_mod_fit_hard_2e22_50ks_41_iter_line_FeKa26abs_flux_0.1_1000_9_in_2p0_10p0_keV_mod_pion_abs_NS_0.005_warm.txt',
+    'photo_nh_lim_mod_fit_hard_2e22_50ks_41_iter_line_FeKa26abs_flux_0.1_1000_9_in_2p0_10p0_keV_mod_pion_abs_NS_0.005_hot.txt',
+    figsize=(6,4))
+
+    '''
+
+
+    val_warm=np.loadtxt(val_csv_warm)
+    val_3sig_warm=val_warm.T[-1]
+    val_flux_warm=val_warm.T[0]
+
+    if val_csv_hot is not None:
+        val_hot=np.loadtxt(val_csv_hot)
+        val_3sig_hot=val_hot.T[-1]
+        val_flux_hot=val_hot.T[0]
+
+
+    edd_conv=edd_factor(d_kpc,M_CO).value
+    # Perform linear regression in log-log space with y in eddington units
+    logslope_warm, logintercept_warm, r_value, p_value, std_err = linregress(np.log10(val_flux_warm*edd_conv),
+                                                                             np.log10(val_3sig_warm))
+
+    #defining the y axis flux range for the warm hard state
+    eddrange_warm_hard=np.logspace(-5,np.log10(ledd_highbranch),200)
+
+    val_pred_warm_hard=10**(logslope_warm*np.log10(eddrange_warm_hard)+logintercept_warm)
+
+
+    #defining the y axis flux range for the warm soft state
+    eddrange_warm_soft=np.logspace(np.log10(ledd_lowbranch),np.log10(ledd_highbranch),100)
+
+    #and computing the predicted limits considering  the soft factor
+    val_pred_warm_soft=10**(logslope_warm*np.log10(eddrange_warm_soft*factor_soft)+logintercept_warm)
+
+
+    #defining the x axis hr range for the state transitions
+    hr_warm_transi=np.logspace(-1-transi_width_margin,0.+transi_width_margin,100)
+    val_pred_warm_transi_high=10**(logslope_warm*\
+                            #normalizing for constant logarithmic step to *factor soft in soft and *1 in hard
+                            np.log10((abs(np.log10(hr_warm_transi))*(factor_soft-1)+1)*ledd_highbranch)
+                            +logintercept_warm)
+
+    val_pred_warm_transi_low=10**(logslope_warm*\
+                            #normalizing for constant logarithmic step to *factor soft in soft and *1 in hard
+                            np.log10((abs(np.log10(hr_warm_transi))*(factor_soft-1)+1)*ledd_lowbranch)
+                            +logintercept_warm)
+
+    vrange_warm=np.log10(np.array([min(val_pred_warm_hard.tolist()+val_pred_warm_soft.tolist()),
+                 max(val_pred_warm_hard.tolist() + val_pred_warm_soft.tolist())]))
+
+
+    '''
+    #hot
+    '''
+    if val_csv_hot is not None:
+        logslope_hot, logintercept_hot, r_value, p_value, std_err = linregress(np.log10(val_flux_hot*edd_conv),
+                                                                                 np.log10(val_3sig_hot))
+
+        #defining the y axis flux range for the hot hard state
+        eddrange_hot_hard=np.logspace(-5,np.log10(ledd_highbranch),200)
+
+        val_pred_hot_hard=10**(logslope_hot*np.log10(eddrange_hot_hard)+logintercept_hot)
+
+
+        #defining the y axis flux range for the hot soft state
+        eddrange_hot_soft=np.logspace(np.log10(ledd_lowbranch),np.log10(ledd_highbranch),100)
+
+        #and computing the predicted limits considering  the soft factor
+        val_pred_hot_soft=10**(logslope_hot*np.log10(eddrange_hot_soft*factor_soft)+logintercept_hot)
+
+
+        #defining the x axis hr range for the state transitions
+        hr_hot_transi=np.logspace(-1-transi_width_margin,0.+transi_width_margin,100)
+        val_pred_hot_transi_high=10**(logslope_hot*\
+                                #normalizing for constant logarithmic step to *factor soft in soft and *1 in hard
+                                np.log10((abs(np.log10(hr_hot_transi))*(factor_soft-1)+1)*ledd_highbranch)
+                                +logintercept_hot)
+
+        val_pred_hot_transi_low=10**(logslope_hot*\
+                                #normalizing for constant logarithmic step to *factor soft in soft and *1 in hard
+                                np.log10((abs(np.log10(hr_hot_transi))*(factor_soft-1)+1)*ledd_lowbranch)
+                                +logintercept_hot)
+
+
+    # fig,ax= plt.subplots(1, 3, figsize=figsize, width_ratios=[1,19, 1])
+    fig,ax= plt.subplots(figsize=figsize)
+
+    ax.set_xscale('log')
+    ax.set_xlabel(r'Spectral Hardness ([6-10]/[3-6] keV flux)')
+    ax.set_xlim(0.05,2)
+
+    ax.set_yscale('log')
+    ax.set_ylabel(r'Luminosity ($L/L_{Edd}$)')
+    ax.set_ylim(1e-5,1)
+
+
+    combined_x_warm=np.repeat(1,len(val_pred_warm_hard)).tolist()+hr_warm_transi.tolist()[::-1]+\
+               np.repeat(0.1,len(val_pred_warm_soft)).tolist()[::-1]+hr_warm_transi.tolist()
+
+    combined_y_warm=eddrange_warm_hard.tolist()+np.repeat(ledd_highbranch,len(hr_warm_transi)).tolist()[::-1]+\
+               eddrange_warm_soft.tolist()[::-1]+np.repeat(ledd_lowbranch,len(hr_warm_transi)).tolist()
+
+    combined_c_warm=np.log10(val_pred_warm_hard).tolist()+np.log10(val_pred_warm_transi_high).tolist()[::-1]+ \
+               np.log10(val_pred_warm_soft).tolist()[::-1]+np.log10(val_pred_warm_transi_low).tolist()
+
+    mask_use_warm = np.array(combined_c_warm) < 0
+    vrange_warm=np.log10(np.array([min(10**np.array(combined_c_warm)[mask_use_warm]),
+                                  max(10**np.array(combined_c_warm)[mask_use_warm])])).round(1)
+
+    lines_warm=colored_line(np.array(combined_x_warm)[mask_use_warm],
+                           np.array(combined_y_warm)[mask_use_warm],
+                           np.array(combined_c_warm)[mask_use_warm],
+                           ax=ax,cmap=col_warm,lw=lw,clim=vrange_warm)
+
+    # lines_warm=colored_line(combined_x_warm,
+    #                         combined_y_warm,
+    #                         combined_c_warm,ax=ax,cmap=col_warm,lw=lw,clim=vrange_warm)
+
+    if val_csv_hot is not None:
+
+        margin_x_hot=0.22
+        combined_x_hot=np.repeat(1*(1+margin_x_hot),len(val_pred_hot_hard)).tolist()+\
+                        [1+margin_x_hot]+\
+                        hr_hot_transi.tolist()[::-1]+\
+                        [0.1/(1+margin_x_hot)] + \
+                        np.repeat(0.1/(1+margin_x_hot),len(val_pred_hot_soft)).tolist()[::-1]+\
+                        hr_hot_transi.tolist()
+
+        combined_y_hot=(1.*eddrange_hot_hard).tolist()+\
+                        [ledd_highbranch*1.8]+\
+                        np.repeat(ledd_highbranch*1.8,len(hr_hot_transi)).tolist()[::-1] +\
+                        [ledd_highbranch * 1.8] +\
+                        ((1.)*eddrange_hot_soft).tolist()[::-1]+\
+                       np.repeat(ledd_lowbranch/2,len(hr_hot_transi)).tolist()
+
+        combined_c_hot=(np.log10(val_pred_hot_hard).tolist()+
+                        [np.log10(val_pred_hot_hard[-1])]+
+                        np.log10(val_pred_hot_transi_high).tolist()[::-1]+
+                        [np.log10(val_pred_hot_soft[-1])]+\
+                   np.log10(val_pred_hot_soft).tolist()[::-1]+np.log10(val_pred_hot_transi_low).tolist())
+
+
+
+        mask_use_hot = np.array(combined_c_hot) < 1
+
+        # breakpoint()
+
+
+        vrange_hot=np.log10(np.array([min(10**np.array(combined_c_hot)[mask_use_hot]),
+                                      max(10**np.array(combined_c_hot)[mask_use_hot])])).round(1)
+
+        lines_hot=colored_line(np.array(combined_x_hot)[mask_use_hot],
+                               np.array(combined_y_hot)[mask_use_hot],
+                               np.array(combined_c_hot)[mask_use_hot],
+                               ax=ax,cmap=col_hot,lw=lw,clim=vrange_hot)
+
+
+        cbar_hot=fig.colorbar(lines_hot,location='left',pad=0.16)
+        cbar_hot.ax.invert_yaxis()
+        cbar_hot_ticks_adj=['%.1e'%elem for elem in 10 ** (22 + cbar_hot.get_ticks())]
+        cbar_hot.set_ticklabels(cbar_hot_ticks_adj)
+        cbar_hot.set_label('hot wind \n NH',y=1.,rotation=0,labelpad=-55)
+        cbar_hot.ax.yaxis.set_ticks_position('left')
+
+
+    # cbar_warm=fig.colorbar(val_warm_hard)
+    cbar_warm=fig.colorbar(lines_warm,pad=0.04,)
+    cbar_warm.ax.invert_yaxis()
+    cbar_warm_ticks_adj=['%.1e'%elem for elem in 10 ** (22 + cbar_warm.get_ticks())]
+    cbar_warm.set_ticklabels(cbar_warm_ticks_adj)
+    cbar_warm.set_label('warm wind \n NH',y=1.1,rotation=0,labelpad=-57)
+
+    plt.tight_layout()
+
+
+
+    # plt.tight_layout()

9e1ddc8a67cb80b6c7086bc80b8cbebd972f1c6a
parrazyte <maxime.parrastro@gmail.com>
2026-09-11 17:42:39 +0900

    setup for this pc specifically

--- observations/spectral_analysis/line_simu/line_simu_outside_launcher.py
+++ observations/spectral_analysis/line_simu/line_simu_outside_launcher.py
@@ -23,7 +23,7 @@ os.chdir('/home/parrazyte/Observ_SA/NewAthena/NHdet')
 # photo_nsteppar_v=20,par_freeze_steppar=[3,5],n_cores=13)
 
 #multi logxi for AMD
-for i in [0.,0.5,1.,1.5,2.5,3.,3.5,4.,4.5,5.,5.5,6.]:
+for i in [0.,0.5,1.,1.5,2.5,3.,3.5,4.,4.5,5.,5.5,6.][::-1]:
     os.chdir('/home/parrazyte/Observ_SA/NewAthena/NHdet')
 
 
@@ -35,4 +35,15 @@ for i in [0.,0.5,1.,1.5,2.5,3.,3.5,4.,4.5,5.,5.5,6.]:
     photo_turb_range=[100,'min',300],photo_v_range=[0,-1000,1000],photo_nsteppar_turb=10,
     photo_nsteppar_v=20,par_freeze_steppar=[3,5],n_cores=13)
 
+#on the main pc
+for i in [0.,0.5,1.,1.5,2.5,3.,3.5,4.,4.5,5.][::-1]:
+    os.chdir('/media/parrazyte/crucial_SSD/Observ/highres/NewAthena/SpecialIssue/NHdet')
 
+
+    line_simu(outdir='AMD_logxi_%.2f'%i,mode='NH_noise_photo',rmf_path='NewAthena_4eV',
+    arf_path='NewAthena_nofilter',bkg_path='NewAthena_NXB_1arcmin',
+    expos=5,n_iter=100,flux_range='1_1_1',flux_band='0.3 10.0',
+    mod_path='SED_soft_0p1Edd_2e22.xcm',set_ener_data=True,set_ener_data_str='large_canon',
+    photo_mod='pion_abs_canon_soft',photo_comp_pos=3,photo_xi_range='%.2f'%i+'_freeze',
+    photo_turb_range=[100,'min',300],photo_v_range=[0,-1000,1000],photo_nsteppar_turb=11,
+    photo_nsteppar_v=21,par_freeze_steppar=[3,5],n_cores=13)
